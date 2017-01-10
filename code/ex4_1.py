a92bab08c93a56b82d8768078a7145d7138cf17f
Oliver Sus <oliver.sus@dwd.de>
2017-01-10 11:47:01 +0100

    test commit

--- code/ex4_1.py
+++ code/ex4_1.py
@@ -7,6 +7,6 @@ live_girls = live[live.babysex == 2]
 cdf_live = thinkstats2.Cdf(live.totalwgt_lb)
 cdf_live_girls = thinkstats2.Cdf(live_girls.totalwgt_lb)
 
-emilia = 3.15 * 2.20462
+emilia = 3.14 * 2.20462
 cdf_live.PercentileRank(emilia)
 cdf_live_girls.PercentileRank(emilia)
